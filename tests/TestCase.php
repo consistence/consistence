7b22606f4347bab3dab6f657ce8a74be6fc83e6b
Vasek Purchart <me@vasekpurchart.cz>
2014-03-02 21:51:51 +0100

    TestCase::ok

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,4 +5,12 @@ namespace Consistence;
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
 
+	/**
+	 * This method can be called to provide at least one assertion in test
+	 */
+	protected function ok()
+	{
+		$this->assertTrue(true);
+	}
+
 }

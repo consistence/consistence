ba4a9859aaca8ee44757471b0666ab306f2206b8
Vasek Purchart <me@vasekpurchart.cz>
2014-03-13 13:39:11 +0100

    hasType

--- src/Type/Type.php
+++ src/Type/Type.php
@@ -5,6 +5,9 @@ namespace Consistence\Type;
 class Type extends \Consistence\ObjectPrototype
 {
 
+	const SUBTYPES_ALLOW = true;
+	const SUBTYPES_DISALLOW = false;
+
 	final public function __construct()
 	{
 		throw new \Consistence\StaticClassException();
@@ -31,6 +34,52 @@ class Type extends \Consistence\ObjectPrototype
 		}
 
 		return $type;
+	}
+
+	/**
+	 * Tests if the $value has one of expected types
+	 *
+	 * Supported syntax:
+	 *  - integer
+	 *  - integer|string
+	 *  - integer|string|float
+	 *  - integer|null
+	 *  - DateTime (do not use leading \)
+	 *  - stdClass|DateTime
+	 *  - integer|DateTime
+	 *  - integer[]
+	 *  - integer[]|string[]
+	 *  - integer[]|DateTime
+	 *  - integer[][]
+	 *
+	 * @param mixed $value
+	 * @param string $expectedTypes
+	 * @param boolean $allowSubtypes decides if subtypes of given expected types should be considered a valid value
+	 * @return boolean
+	 */
+	public static function hasType($value, $expectedTypes, $allowSubtypes = self::SUBTYPES_ALLOW)
+	{
+		$types = explode('|', $expectedTypes);
+		foreach ($types as $type) {
+			$typeLength = strlen($type);
+			if ($type[$typeLength - 1] === ']' && $type[$typeLength - 2] === '[') {
+				if (!is_array($value)) {
+					continue; // skip to next type
+				}
+				foreach ($value as $item) {
+					if (!self::hasType($item, substr($type, 0, $typeLength - 2))) {
+						continue 2; // skip to next type
+					}
+				}
+
+				return true;
+			}
+			if (strcasecmp(self::getType($value), $type) === 0 || ($allowSubtypes && is_a($value, $type))) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 }
--- tests/Type/TypeTest.php
+++ tests/Type/TypeTest.php
@@ -31,6 +31,40 @@ class TypeTest extends \Consistence\TestCase
 	}
 
 	/**
+	 * @return mixed[][]
+	 */
+	public function typeChecksProvider()
+	{
+		return array_merge(
+			$this->typesProvider(),
+			[
+				[null, 'NULL'],
+				[1, 'string|integer'],
+				['foo', 'string|integer'],
+				[2, 'null|integer'],
+				[null, 'null|integer'],
+				[null, 'NULL|integer'],
+				[DateTimeImmutable::class, 'DateTimeImmutable|string'],
+				[new DateTimeImmutable(), 'DateTimeImmutable|string'],
+				[[], 'string[]'],
+				['foo', 'string[]', false],
+				[['foo'], 'string[]'],
+				[[1], 'string[]', false],
+				[['foo', 'bar'], 'string[]'],
+				[[], 'string[]|integer[]'],
+				[['foo', 'bar'], 'string[]|integer[]'],
+				[2, 'string[]|integer'],
+				[[1, 2], 'string[]|integer[]'],
+				[[[1, 2]], 'integer[][]'],
+				[[[1, 2], [3, 4]], 'integer[][]'],
+				[[[1, 2], ['foo']], 'integer[][]', false],
+				[[['foo']], 'integer[][]', false],
+				[[1, 2], 'integer[][]', false],
+			]
+		);
+	}
+
+	/**
 	 * @dataProvider typesProvider
 	 *
 	 * @param mixed $type
@@ -39,6 +73,18 @@ class TypeTest extends \Consistence\TestCase
 	public function testTypes($type, $expected)
 	{
 		$this->assertSame($expected, Type::getType($type));
+	}
+
+	/**
+	 * @dataProvider typeChecksProvider
+	 *
+	 * @param mixed $value
+	 * @param string $expectedTypes
+	 * @param boolean $result
+	 */
+	public function testHasType($value, $expectedTypes, $result = true)
+	{
+		$this->assertSame($result, Type::hasType($value, $expectedTypes));
 	}
 
 }

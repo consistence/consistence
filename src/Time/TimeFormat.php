79bb0ce6b8dfb3390bf95119d30fe64f5e4e68c2
Vasek Purchart <me@vasekpurchart.cz>
2014-05-19 14:54:00 +0200

    PHP date/time constants

---
+++ src/Time/TimeFormat.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Consistence\Time;
+
+/**
+ * @see http://php.net/manual/en/function.date.php#refsect1-function.date-parameters
+ * @see http://www.php.net/manual/en/class.datetime.php#datetime.constants.types
+ */
+class TimeFormat extends \Consistence\ObjectPrototype
+{
+
+	const ATOM = DATE_ATOM;
+	const COOKIE = DATE_COOKIE;
+	const ISO8601 = DATE_ISO8601;
+	const ISO8601_TIMEZONE_WITH_COLON = DATE_RFC3339;
+	const RFC822 = DATE_RFC822;
+	const RFC850 = DATE_RFC850;
+	const RFC1036 = DATE_RFC1036;
+	const RFC1123 = DATE_RFC1123;
+	const RFC2822 = DATE_RFC2822;
+	const RFC3339 = DATE_RFC3339;
+	const RSS = DATE_RSS;
+	const W3C = DATE_W3C;
+
+	const DAY_OF_MONTH = 'j';
+	const DAY_OF_MONTH_LEADING_ZERO = 'd';
+	const DAY_OF_WEEK = 'w';
+	const DAY_OF_WEEK_ISO8601 = 'N';
+	const DAY_OF_WEEK_TEXT_FULL = 'l';
+	const DAY_OF_WEEK_TEXT_THREE_LETTERS = 'D';
+	const DAY_OF_YEAR = 'z';
+	const DAYS_IN_MONTH = 't';
+	const MICROSECOND_OF_SECOND = 'u';
+	const MINUTE_OF_HOUR_LEADING_ZERO = 'i';
+	const MONTH_OF_YEAR = 'n';
+	const MONTH_OF_YEAR_LEADING_ZERO = 'm';
+	const MONTH_OF_YEAR_TEXT_FULL = 'F';
+	const MONTH_OF_YEAR_THREE_LETTERS = 'M';
+	const SECOND_OF_MINUTE_LEADING_ZERO = 's';
+	const TIME_OF_DAY = 'G';
+	const TIME_OF_DAY_LEADING_ZERO = 'H';
+	const TIME_OF_DAY_12_HOUR = 'g';
+	const TIME_OF_DAY_12_HOUR_LEADING_ZERO = 'h';
+	const TIME_OF_DAY_AM_PM = 'a';
+	const TIME_OF_DAY_AM_PM_UPPERCASE = 'A';
+	const TIME_OF_DAY_SWATCH = 'B';
+	const TIMEZONE = 'e';
+	const TIMEZONE_ABBREVIATION = 'T';
+	const TIMEZONE_IS_DAYLIGHT = 'I';
+	const TIMEZONE_OFFSET = 'O';
+	const TIMEZONE_OFFSET_SECONDS = 'Z';
+	const TIMEZONE_OFFSET_WITH_COLON = 'P';
+	const UNIX_TIMESTAMP = 'U';
+	const WEEK_OF_YEAR = 'W';
+	const YEAR = 'Y';
+	const YEAR_IS_LEAP = 'L';
+	const YEAR_ISO8601 = 'o';
+	const YEAR_TWO_DIGIT = 'y';
+
+}

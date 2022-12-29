93ca42472aacdb851f99ba2805970adaf9b8b4b1
Ondřej Michálek <michondr@gmail.com>
2022-12-29 18:45:19 +0100

    temp rename dataproviders

--- tests/Type/TypeTest.php
+++ tests/Type/TypeTest.php
@@ -26,41 +26,41 @@ class TypeTest extends \Consistence\TestCase
 	public function typesProvider(): array
 	{
 		return [
-			[
+			'DP #1' => [
 				'value' => 'foo',
 				'expectedType' => 'string',
 			],
-			[
+			'DP #2' => [
 				'value' => 1,
 				'expectedType' => 'int',
 			],
-			[
+			'DP #3' => [
 				'value' => true,
 				'expectedType' => 'bool',
 			],
-			[
+			'DP #4' => [
 				'value' => 1.5,
 				'expectedType' => 'float',
 			],
-			[
+			'DP #5' => [
 				'value' => [],
 				'expectedType' => 'array',
 			],
-			[
+			'DP #6' => [
 				'value' => null,
 				'expectedType' => 'null',
 			],
-			[
+			'DP #7' => [
 				'value' => new DateTimeImmutable(),
 				'expectedType' => DateTimeImmutable::class,
 			],
-			[
+			'DP #8' => [
 				'value' => static function (): void {
 					return;
 				},
 				'expectedType' => Closure::class,
 			],
-			[
+			'DP #9' => [
 				'value' => fopen(__DIR__, 'r'),
 				'expectedType' => 'resource',
 			],
@@ -73,445 +73,445 @@ class TypeTest extends \Consistence\TestCase
 	public function typeChecksProvider(): array
 	{
 		return [
-			[
+			'DP #1' => [
 				'value' => null,
 				'expectedType' => 'NULL',
 			],
-			[
+			'DP #2' => [
 				'value' => 1,
 				'expectedType' => 'string|int',
 			],
-			[
+			'DP #3' => [
 				'value' => 1,
 				'expectedType' => 'string|integer',
 			],
-			[
+			'DP #4' => [
 				'value' => 'foo',
 				'expectedType' => 'string|int',
 			],
-			[
+			'DP #5' => [
 				'value' => 2,
 				'expectedType' => 'null|int',
 			],
-			[
+			'DP #6' => [
 				'value' => 2,
 				'expectedType' => 'null|integer',
 			],
-			[
+			'DP #7' => [
 				'value' => true,
 				'expectedType' => 'null|bool',
 			],
-			[
+			'DP #8' => [
 				'value' => false,
 				'expectedType' => 'bool|null',
 			],
-			[
+			'DP #9' => [
 				'value' => null,
 				'expectedType' => 'null|int',
 			],
-			[
+			'DP #10' => [
 				'value' => null,
 				'expectedType' => 'NULL|int',
 			],
-			[
+			'DP #11' => [
 				'value' => null,
 				'expectedType' => 'NULL|integer',
 			],
-			[
+			'DP #12' => [
 				'value' => DateTimeImmutable::class,
 				'expectedType' => 'DateTimeImmutable|string',
 			],
-			[
+			'DP #13' => [
 				'value' => new DateTimeImmutable(),
 				'expectedType' => 'DateTimeImmutable|string',
 			],
-			[
+			'DP #14' => [
 				'value' => new DateTimeImmutable(),
 				'expectedType' => 'object',
 			],
-			[
+			'DP #15' => [
 				'value' => 'foo',
 				'expectedType' => 'object',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #16' => [
 				'value' => 1,
 				'expectedType' => 'mixed',
 			],
-			[
+			'DP #17' => [
 				'value' => 'foo',
 				'expectedType' => 'mixed',
 			],
-			[
+			'DP #18' => [
 				'value' => DateTimeImmutable::class,
 				'expectedType' => 'mixed',
 			],
-			[
+			'DP #19' => [
 				'value' => 1,
 				'expectedType' => 'string|mixed',
 			],
-			[
+			'DP #20' => [
 				'value' => [],
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #21' => [
 				'value' => 'foo',
 				'expectedType' => 'string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #22' => [
 				'value' => ['foo'],
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #23' => [
 				'value' => [1],
 				'expectedType' => 'string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #24' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #25' => [
 				'value' => [],
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #26' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #27' => [
 				'value' => 2,
 				'expectedType' => 'string[]|int',
 			],
-			[
+			'DP #28' => [
 				'value' => [1, 2],
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #29' => [
 				'value' => [new DateTimeImmutable()],
 				'expectedType' => 'object[]',
 			],
-			[
+			'DP #30' => [
 				'value' => [new DateTimeImmutable(), new stdClass()],
 				'expectedType' => 'object[]',
 			],
-			[
+			'DP #31' => [
 				'value' => [new DateTimeImmutable(), 'foo'],
 				'expectedType' => 'object[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #32' => [
 				'value' => [1, 'foo'],
 				'expectedType' => 'mixed[]',
 			],
-			[
+			'DP #33' => [
 				'value' => 1,
 				'expectedType' => 'mixed[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #34' => [
 				'value' => [1, 'foo'],
 				'expectedType' => 'mixed',
 			],
-			[
+			'DP #35' => [
 				'value' => [[1, 2]],
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #36' => [
 				'value' => [[1, 2], [3, 4]],
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #37' => [
 				'value' => [[1, 2], [3, 4]],
 				'expectedType' => 'integer[][]',
 			],
-			[
+			'DP #38' => [
 				'value' => [[1, 2], ['foo']],
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #39' => [
 				'value' => [['foo']],
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #40' => [
 				'value' => [1, 2],
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #41' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #42' => [
 				'value' => new ArrayObject(['foo']),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #43' => [
 				'value' => new ArrayObject([1]),
 				'expectedType' => 'string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #44' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #45' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #46' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #47' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #48' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2])]),
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #49' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject([3, 4])]),
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #50' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject(['foo'])]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #51' => [
 				'value' => new ArrayObject([new ArrayObject(['foo'])]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #52' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #53' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #54' => [
 				'value' => new ArrayObject(['foo']),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #55' => [
 				'value' => new ArrayObject([1]),
 				'expectedType' => 'string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #56' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'string[]',
 			],
-			[
+			'DP #57' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #58' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #59' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'string[]|int[]',
 			],
-			[
+			'DP #60' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2])]),
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #61' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject([3, 4])]),
 				'expectedType' => 'int[][]',
 			],
-			[
+			'DP #62' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject(['foo'])]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #63' => [
 				'value' => new ArrayObject([new ArrayObject(['foo'])]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #64' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #65' => [
 				'value' => [],
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #66' => [
 				'value' => 'foo',
 				'expectedType' => 'int:string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #67' => [
 				'value' => ['foo'],
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #68' => [
 				'value' => [1],
 				'expectedType' => 'int:string[]',
 				'isTheSameType' =>false,
 			],
-			[
+			'DP #69' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #70' => [
 				'value' => [],
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #71' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #72' => [
 				'value' => [1, 2],
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #73' => [
 				'value' => [1, 'foo'],
 				'expectedType' => 'int:mixed[]',
 			],
-			[
+			'DP #74' => [
 				'value' => 1,
 				'expectedType' => 'int:mixed[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #75' => [
 				'value' => [[1, 2]],
 				'expectedType' => 'int:int:int[][]',
 			],
-			[
+			'DP #76' => [
 				'value' => [[1, 2], [3, 4]],
 				'expectedType' => 'int:int:int[][]',
 			],
-			[
+			'DP #77' => [
 				'value' => [[1, 2], ['foo']],
 				'expectedType' => 'int:int:int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #78' => [
 				'value' => [['foo']],
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #79' => [
 				'value' => [1, 2],
 				'expectedType' => 'int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #80' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #81' => [
 				'value' => new ArrayObject(['foo']),
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #82' => [
 				'value' => new ArrayObject([1]),
 				'expectedType' => 'int:string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #83' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'int:string[]',
 			],
-			[
+			'DP #84' => [
 				'value' => new ArrayObject([]),
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #85' => [
 				'value' => new ArrayObject(['foo', 'bar']),
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #86' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'int:string[]|int:int[]',
 			],
-			[
+			'DP #87' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2])]),
 				'expectedType' => 'int:int:int[][]',
 			],
-			[
+			'DP #88' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject([3, 4])]),
 				'expectedType' => 'int:int:int[][]',
 			],
-			[
+			'DP #89' => [
 				'value' => new ArrayObject([new ArrayObject([1, 2]), new ArrayObject(['foo'])]),
 				'expectedType' => 'int:int:int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #90' => [
 				'value' => new ArrayObject([new ArrayObject(['foo'])]),
 				'expectedType' => 'int:int:int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #91' => [
 				'value' => new ArrayObject([1, 2]),
 				'expectedType' => 'int:int:int[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #92' => [
 				'value' => ['foo' => 'bar'],
 				'expectedType' => 'string:string[]',
 			],
-			[
+			'DP #93' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'string:string[]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #94' => [
 				'value' => ['foo', 'bar'],
 				'expectedType' => 'string:string[]|int:string[]',
 			],
-			[
+			'DP #95' => [
 				'value' => ['foo' => 'bar'],
 				'expectedType' => 'string:string[]|int:string[]',
 			],
-			[
+			'DP #96' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'string:int:string[][]',
 			],
-			[
+			'DP #97' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'string:string:string[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #98' => [
 				'value' => [['foo' => 'bar']],
 				'expectedType' => 'int:string:string[][]',
 			],
-			[
+			'DP #99' => [
 				'value' => [['foo' => 'bar']],
 				'expectedType' => 'int:int:string[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #100' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'string:string[][]',
 			],
-			[
+			'DP #101' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'int:string[][]',
 				'isTheSameType' => false,
 			],
-			[
+			'DP #102' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'mixed:int:string[][]',
 			],
-			[
+			'DP #103' => [
 				'value' => ['foo' => ['bar']],
 				'expectedType' => 'mixed:string:string[][]',
 				'isTheSameType' => false,
